56ec46413aab4a3be3f401c154306378eb7625b7
root <hien@tibbo.com>
2020-10-16 13:42:17 +0800

    docker_test_20201016_134217

--- chroot_rundisk.sh
+++ chroot_rundisk.sh
@@ -3,28 +3,32 @@
 echo -e "\r"
 echo -e "---Defining Environmental variables---"
 echo -e "\r"
+
+build_BOOOT_BIN_filename="build_BOOOT_BIN.sh"
+disk_foldername="disk"
+scripts_foldername="scripts"
+SP7xxx_foldername="SP7021"
+
 home_dir=~
 usr_dir=/usr
 scripts_dir=/scripts
 usr_bin_dir=${usr_dir}/bin
-scripts_foldername="scripts"
-home_scripts_dir=${home_dir}/${scripts_foldername}
-sunplus_foldername="SP7021"
-sunplus_dir=${home_dir}/${sunplus_foldername}
-initramfs_dir=${sunplus_dir}/linux/rootfs/initramfs
-disk_foldername="disk"
-disk_dir=${initramfs_dir}/${disk_foldername}
-disk_scripts_dir=${disk_dir}/${scripts_foldername}
+home_lttp3rootfs_dir=${home_dir}/LTPP3_ROOTFS
+
+SP7xxx_dir=${home_dir}/${SP7xxx_foldername}
+SP7xxx_linux_rootfs_initramfs_dir=${SP7xxx_dir}/linux/rootfs/initramfs
+SP7xxx_linux_rootfs_initramfs_disk_dir=${SP7xxx_linux_rootfs_initramfs_dir}/${disk_foldername}
+SP7xxx_linux_rootfs_initramfs_disk_scripts_dir=${SP7xxx_linux_rootfs_initramfs_disk_dir}/${scripts_foldername}
+
 qemu_fpath=${usr_bin_dir}/qemu-arm-static
 bash_fpath=${usr_bin_dir}/bash
 
 chroot_exec_cmd_inside_chroot_filename="chroot_exec_cmd_inside_chroot.sh"
-scripts_chroot_exec_cmd_inside_chroot_fpath=${home_scripts_dir}/${chroot_exec_cmd_inside_chroot_filename}
-disk_scripts_chroot_exec_cmd_inside_chroot_fpath=${disk_scripts_dir}/${chroot_exec_cmd_inside_chroot_filename}
-chroot_scripts_chroot_exec_cmd_inside_chroot_fpath=${scripts_dir}/${chroot_exec_cmd_inside_chroot_filename}
+src_chroot_exec_cmd_inside_chroot_fpath=${home_lttp3rootfs_dir}/${chroot_exec_cmd_inside_chroot_filename}
+dst_chroot_exec_cmd_inside_chroot_fpath=${SP7xxx_linux_rootfs_initramfs_disk_scripts_dir}/${chroot_exec_cmd_inside_chroot_filename}
+chroot_exec_cmd_inside_chroot_fpath=${scripts_dir}/${chroot_exec_cmd_inside_chroot_filename}
 
-build_BOOOT_BIN_filename="build_BOOOT_BIN.sh"
-scripts_build_BOOOT_BIN_fpath=${home_scripts_dir}/${build_BOOOT_BIN_filename}
+build_BOOOT_BIN_fpath=${home_lttp3rootfs_dir}/${build_BOOOT_BIN_filename}
 
 #---Show mmessage
 echo -e "\r"
@@ -35,25 +39,25 @@ echo -e "---------------------------------------------------------------"
 #---Create directory "~/SP7021/linux/rootfs/initramfs/disk/scripts"
 echo -e "\r"
 echo -e ">Creating <${scripts_foldername}>"
-echo -e ">in: ${disk_dir}"
-if [[ ! -d ${disk_scripts_dir} ]]; then
-	mkdir ${disk_scripts_dir}
+echo -e ">in: ${SP7xxx_linux_rootfs_initramfs_disk_dir}"
+if [[ ! -d ${SP7xxx_linux_rootfs_initramfs_disk_scripts_dir} ]]; then
+	mkdir ${SP7xxx_linux_rootfs_initramfs_disk_scripts_dir}
 fi
 
 #---Copy "chroot_exec_cmd_inside_chroot.sh" to "~/SP7021/linux/rootfs/initramfs/disk/scripts"
 echo -e "\r"
 echo -e ">Copying: ${chroot_exec_cmd_inside_chroot_filename}"
-echo -e ">from: ${home_scripts_dir}"
-echo -e ">to: ${disk_scripts_dir}"
+echo -e ">from: ${home_lttp3rootfs_dir}"
+echo -e ">to: ${SP7xxx_linux_rootfs_initramfs_disk_scripts_dir}"
 echo -e "\r"
-cp ${scripts_chroot_exec_cmd_inside_chroot_fpath} ${disk_scripts_dir}
+cp ${src_chroot_exec_cmd_inside_chroot_fpath} ${SP7xxx_linux_rootfs_initramfs_disk_scripts_dir}
 
 
 #---Make "chroot_exec_cmd_inside_chroot.sh" executable
 echo -e "\r"
 echo -e ">chmod +x ${chroot_exec_cmd_inside_chroot_filename}"
-echo -e ">in: ${disk_scripts_dir}"
-chmod +x ${disk_scripts_chroot_exec_cmd_inside_chroot_fpath}
+echo -e ">in: ${SP7xxx_linux_rootfs_initramfs_disk_scripts_dir}"
+chmod +x ${dst_chroot_exec_cmd_inside_chroot_fpath}
 
 
 #---Go into CHROOT
@@ -70,10 +74,10 @@ if [[ ${answer} == "n" ]] || [[ ${answer} == "N" ]]; then
 	echo -e "---------------------------------------------------------------"
 	echo -e "To Manually Initialize and Prepare <rootfs>..."
 	echo -e "...run the following script:"
-	echo -e "\t${chroot_scripts_chroot_exec_cmd_inside_chroot_fpath}"
+	echo -e "\t${chroot_exec_cmd_inside_chroot_fpath}"
 	echo -e "---------------------------------------------------------------"
 	echo -e "\r"
- 	chroot ${disk_dir} ${qemu_fpath} ${bash_fpath}
+ 	chroot ${SP7xxx_linux_rootfs_initramfs_disk_dir} ${qemu_fpath} ${bash_fpath}
 		
 	exit
 fi
@@ -83,25 +87,25 @@ fi
 echo -e "---------------------------------------------------------------"
 echo -e "Auto Initialization and Preparation of <rootfs>...In Progress"
 echo -e "Running script:"
-echo -e "\t${chroot_scripts_chroot_exec_cmd_inside_chroot_fpath}"
+echo -e "\t${chroot_exec_cmd_inside_chroot_fpath}"
 echo -e "---------------------------------------------------------------"
 echo -e "\r"
 
-cat << EOF | chroot ${disk_dir} ${qemu_fpath} ${bash_fpath}
-	source ${chroot_scripts_chroot_exec_cmd_inside_chroot_fpath}
+cat << EOF | chroot ${SP7xxx_linux_rootfs_initramfs_disk_dir} ${qemu_fpath} ${bash_fpath}
+	source ${chroot_exec_cmd_inside_chroot_fpath}
 EOF
 
 echo -e "---------------------------------------------------------------"
 echo -e "Auto Initialization and Preparation of <rootfs>...Completed"
 echo -e "\r"
 echo -e "To BUILD the <ISPBOOOT.BIN>, please run the following command:"
-echo -e "\t${scripts_build_BOOOT_BIN_fpath}"
+echo -e "\t${build_BOOOT_BIN_fpath}"
 echo -e "---------------------------------------------------------------"
 echo -e "\r"
 
 #---Enter CHROOT
 echo -e "\r"
 echo -e ">Removing: ${chroot_exec_cmd_inside_chroot_filename}"
-echo -e ">from: ${disk_scripts_dir}"
+echo -e ">from: ${SP7xxx_linux_rootfs_initramfs_disk_scripts_dir}"
 echo -e "\r"
-rm ${disk_scripts_chroot_exec_cmd_inside_chroot_fpath}
+rm ${dst_chroot_exec_cmd_inside_chroot_fpath}
