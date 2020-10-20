69d6fc1314aec2065c0cbe5cd1ceb7f6af8ce20c
hien <hien@tibbo.com>
2020-10-20 16:40:41 +0800

    docker_test_20201020_164041

--- chroot_rundisk_2.sh
+++ chroot_rundisk_2.sh
@@ -1,6 +1,19 @@
-qemu_fpath=/usr/bin/qemu-arm-static
-bash_fpath=/usr/bin/bash
+disk_foldername="disk"
+scripts_foldername="scripts"
+SP7xxx_foldername="SP7021"
 
-cat << EOF | chroot ${qemu_fpath} ${bash_fpath}
+home_dir=~
+usr_dir=/usr
+scripts_dir=/scripts
+usr_bin_dir=${usr_dir}/bin
+
+SP7xxx_dir=${home_dir}/${SP7xxx_foldername}
+SP7xxx_linux_rootfs_initramfs_dir=${SP7xxx_dir}/linux/rootfs/initramfs
+SP7xxx_linux_rootfs_initramfs_disk_dir=${SP7xxx_linux_rootfs_initramfs_dir}/${disk_foldername}
+
+qemu_fpath=${usr_bin_dir}/qemu-arm-static
+bash_fpath=${usr_bin_dir}/bash
+
+cat << EOF | chroot ${SP7xxx_linux_rootfs_initramfs_disk_dir} ${qemu_fpath} ${bash_fpath}
 	apt-get install -y bsdmainutils
 EOF
