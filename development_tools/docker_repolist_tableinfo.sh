ac93dacdcf500b909f8939650f215f4b9c6edf54
imcase <hien@tibbo.com>
2023-05-18 10:11:31 +0800

    repolist can show 'Exited' status now

--- development_tools/docker_repolist_tableinfo.sh
+++ development_tools/docker_repolist_tableinfo.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-#---ENVIRONMENT VARIABLES
-docker__tmp__dir="/tmp"
-docker__docker_repoList_tmp__filename="docker__docker_repoList.tmp"
-docker__docker_repoList_tmp__fpath=${docker__tmp__dir}/${docker__docker_repoList_tmp__filename}
-docker__docker_repoList_print__filename="docker__docker_repoList.prn"
-docker__docker_repoList_print__fpath=${docker__tmp__dir}/${docker__docker_repoList_print__filename}
-
-
-
 #---FUNCTIONS
 function containerIsActive__func() {
 	#Input args
@@ -18,9 +9,21 @@ function containerIsActive__func() {
 	local repo_tag="${repoName__input}:${tag__input}"
 
 	#Check if imageID is found in container's list
-	local stdOutput=`docker ps -a | grep "${repo_tag}"`
-	if [[ ! -z ${stdOutput} ]]; then
-		echo "true"
+	#Remarks:
+	#	{{.Image}}: get the 'repository:tag'
+	#	{{.Status}}: get the 'status'
+	local result=$(docker ps -a --format "table {{.Image}} {{.Status}}" | grep "${repo_tag}")
+	if [[ -n "${result}" ]]; then
+		#Remarks:
+		#	sed 's/ /_/g': replace SPACE with UNDERSCORE
+		#	sed 's/(.*.)//g': replace ANYTHING that is BETWEEN BRACKETS (including the brackets) with EMPTY STRING
+		#	sed 's/__/_/g'): replace DOUBLE UNDERSCORE with UNDERSCORE
+		local isExited=$(grep -o "${PATTERN_EXITED}.*" <<< "${result}" | sed 's/ /_/g' | sed 's/(.*.)//g' | sed 's/__/_/g')
+		if [[ -n "${isExited}" ]]; then
+			echo "${isExited}"
+		else
+			echo "true"
+		fi
 	else
 		echo "false"
 	fi
@@ -29,6 +32,275 @@ function containerIsActive__func() {
 
 
 #---SUBROUTINES
+docker__get_source_fullpath__sub() {
+    #Define constants
+    local PHASE_CHECK_CACHE=1
+    local PHASE_FIND_PATH=10
+    local PHASE_EXIT=100
+
+    #Define variables
+    local phase=""
+
+    local current_dir=""
+    local parent_dir=""
+    local search_dir=""
+    local tmp_dir=""
+
+    local development_tools_foldername=""
+    local lTPP3_ROOTFS_foldername=""
+    local global_filename=""
+    local parentDir_of_LTPP3_ROOTFS_dir=""
+
+    local mainmenu_path_cache_filename=""
+    local mainmenu_path_cache_fpath=""
+
+    local find_dir_result_arr=()
+    local find_dir_result_arritem=""
+
+    local path_of_development_tools_found=""
+    local parentpath_of_development_tools=""
+
+    local isfound=""
+
+    local retry_ctr=0
+
+    #Set variables
+    phase="${PHASE_CHECK_CACHE}"
+    current_dir=$(dirname $(readlink -f $0))
+    parent_dir="$(dirname "${current_dir}")"
+    tmp_dir=/tmp
+    development_tools_foldername="development_tools"
+    global_filename="docker_global.sh"
+    lTPP3_ROOTFS_foldername="LTPP3_ROOTFS"
+
+    mainmenu_path_cache_filename="docker__mainmenu_path.cache"
+    mainmenu_path_cache_fpath="${tmp_dir}/${mainmenu_path_cache_filename}"
+
+    result=false
+
+    #Start loop
+    while true
+    do
+        case "${phase}" in
+            "${PHASE_CHECK_CACHE}")
+                if [[ -f "${mainmenu_path_cache_fpath}" ]]; then
+                    #Get the directory stored in cache-file
+                    docker__LTPP3_ROOTFS_development_tools__dir=$(awk 'NR==1' "${mainmenu_path_cache_fpath}")
+
+                    #Move one directory up
+                    parentpath_of_development_tools=$(dirname "${docker__LTPP3_ROOTFS_development_tools__dir}")
+
+                    #Check if 'development_tools' is in the 'LTPP3_ROOTFS' folder
+                    isfound=$(docker__checkif_paths_are_related "${current_dir}" \
+                            "${parentpath_of_development_tools}" "${lTPP3_ROOTFS_foldername}")
+                    if [[ ${isfound} == false ]]; then
+                        phase="${PHASE_FIND_PATH}"
+                    else
+                        result=true
+
+                        phase="${PHASE_EXIT}"
+                    fi
+                else
+                    phase="${PHASE_FIND_PATH}"
+                fi
+                ;;
+            "${PHASE_FIND_PATH}")   
+                #Print
+                echo -e "---:\e[30;38;5;215mSTART\e[0;0m: find path of folder \e[30;38;5;246m'${development_tools_foldername}\e[0;0m"
+
+                #Initialize variables
+                docker__LTPP3_ROOTFS_development_tools__dir=""
+                search_dir="${current_dir}"   #start with search in the current dir
+
+                #Start loop
+                while true
+                do
+                    #Get all the directories containing the foldername 'LTPP3_ROOTFS'...
+                    #... and read to array 'find_result_arr'
+                    readarray -t find_dir_result_arr < <(find  "${search_dir}" -type d -iname "${lTPP3_ROOTFS_foldername}" 2> /dev/null)
+
+                    #Iterate thru each array-item
+                    for find_dir_result_arritem in "${find_dir_result_arr[@]}"
+                    do
+                        echo -e "---:\e[30;38;5;215mCHECKING\e[0;0m: ${find_dir_result_arritem}"
+
+                        #Find path
+                        isfound=$(docker__checkif_paths_are_related "${current_dir}" \
+                                "${find_dir_result_arritem}"  "${lTPP3_ROOTFS_foldername}")
+                        if [[ ${isfound} == true ]]; then
+                            #Update variable 'path_of_development_tools_found'
+                            path_of_development_tools_found="${find_dir_result_arritem}/${development_tools_foldername}"
+
+                            #Check if 'directory' exist
+                            if [[ -d "${path_of_development_tools_found}" ]]; then    #directory exists
+                                #Update variable
+                                #Remark:
+                                #   'docker__LTPP3_ROOTFS_development_tools__dir' is a global variable.
+                                #   This variable will be passed 'globally' to script 'docker_global.sh'.
+                                docker__LTPP3_ROOTFS_development_tools__dir="${path_of_development_tools_found}"
+
+                                break
+                            fi
+                        fi
+                    done
+
+                    #Check if 'docker__LTPP3_ROOTFS_development_tools__dir' contains any data
+                    if [[ -z "${docker__LTPP3_ROOTFS_development_tools__dir}" ]]; then  #contains no data
+                        case "${retry_ctr}" in
+                            0)
+                                search_dir="${parent_dir}"    #next search in the 'parent' directory
+                                ;;
+                            1)
+                                search_dir="/" #finally search in the 'main' directory (the search may take longer)
+                                ;;
+                            *)
+                                echo -e "\r"
+                                echo -e "***\e[1;31mERROR\e[0;0m: folder \e[30;38;5;246m${development_tools_foldername}\e[0;0m: \e[30;38;5;131mNot Found\e[0;0m"
+                                echo -e "\r"
+
+                                #Update variable
+                                result=false
+
+                                #set phase
+                                phase="${PHASE_EXIT}"
+
+                                break
+                                ;;
+                        esac
+
+                        ((retry_ctr++))
+                    else    #contains data
+                        #Print
+                        echo -e "---:\e[30;38;5;215mCOMPLETED\e[0;0m: find path of folder \e[30;38;5;246m'${development_tools_foldername}\e[0;0m"
+
+
+                        #Write to file
+                        echo "${docker__LTPP3_ROOTFS_development_tools__dir}" | tee "${mainmenu_path_cache_fpath}" >/dev/null
+
+                        #Print
+                        echo -e "---:\e[30;38;5;215mSTATUS\e[0;0m: write path to temporary cache-file: \e[1;33mDONE\e[0;0m"
+
+                        #Update variable
+                        result=true
+
+                        #set phase
+                        phase="${PHASE_EXIT}"
+
+                        break
+                    fi
+                done
+                ;;    
+            "${PHASE_EXIT}")
+                break
+                ;;
+        esac
+    done
+
+    #Exit if 'result = false'
+    if [[ ${result} == false ]]; then
+        exit 99
+    fi
+
+    #Retrieve directories
+    #Remark:
+    #   'docker__LTPP3_ROOTFS__dir' is a global variable.
+    #   This variable will be passed 'globally' to script 'docker_global.sh'.
+    docker__LTPP3_ROOTFS__dir=${docker__LTPP3_ROOTFS_development_tools__dir%/*}    #move one directory up: LTPP3_ROOTFS/
+    parentDir_of_LTPP3_ROOTFS_dir=${docker__LTPP3_ROOTFS__dir%/*}    #move two directories up. This directory is the one-level higher than LTPP3_ROOTFS/
+
+    #Get full-path
+    #Remark:
+    #   'docker__global__fpath' is a global variable.
+    #   This variable will be passed 'globally' to script 'docker_global.sh'.
+    docker__global__fpath=${docker__LTPP3_ROOTFS_development_tools__dir}/${global_filename}
+}
+docker__checkif_paths_are_related() {
+    #Input args
+    local scriptdir__input=${1}
+    local finddir__input=${2}
+    local pattern__input=${3}
+
+    #Define constants
+    local PHASE_PATTERN_CHECK1=1
+    local PHASE_PATTERN_CHECK2=10
+    local PHASE_PATH_COMPARISON=20
+    local PHASE_EXIT=100
+
+    #Define variables
+    local phase="${PHASE_PATTERN_CHECK1}"
+    local isfound1=""
+    local isfound2=""
+    local isfound3=""
+    local ret=false
+
+    while true
+    do
+        case "${phase}" in
+            "${PHASE_PATTERN_CHECK1}")
+                #Check if 'pattern__input' is found in 'scriptdir__input'
+                isfound1=$(echo "${scriptdir__input}" | \
+                        grep -o "${pattern__input}.*" | \
+                        cut -d"/" -f1 | grep -w "^${pattern__input}$")
+                if [[ -z "${isfound1}" ]]; then
+                    ret=false
+
+                    phase="${PHASE_EXIT}"
+                else
+                    phase="${PHASE_PATTERN_CHECK2}"
+                fi                
+                ;;
+            "${PHASE_PATTERN_CHECK2}")
+                #Check if 'pattern__input' is found in 'finddir__input'
+                isfound2=$(echo "${finddir__input}" | \
+                        grep -o "${pattern__input}.*" | \
+                        cut -d"/" -f1 | grep -w "^${pattern__input}$")
+                if [[ -z "${isfound2}" ]]; then
+                    ret=false
+
+                    phase="${PHASE_EXIT}"
+                else
+                    phase="${PHASE_PATH_COMPARISON}"
+                fi                
+                ;;
+            "${PHASE_PATH_COMPARISON}")
+                #Check if 'development_tools' is under the folder 'LTPP3_ROOTFS'
+                isfound3=$(echo "${scriptdir__input}" | \
+                        grep -w "${finddir__input}.*")
+                if [[ -z "${isfound3}" ]]; then
+                    ret=false
+                else
+                    ret=true
+                fi
+
+                phase="${PHASE_EXIT}"
+                ;;
+            "${PHASE_EXIT}")
+                break
+                ;;
+        esac
+    done
+
+    #Output
+    echo "${ret}"
+
+    return 0
+}
+docker__load_global_fpath_paths__sub() {
+    source ${docker__global__fpath}
+}
+
+docker__environmental_variables__sub() {
+	docker__tmp__dir="/tmp"
+	docker__docker_repoList_tmp__filename="docker__docker_repoList.tmp"
+	docker__docker_repoList_tmp__fpath=${docker__tmp__dir}/${docker__docker_repoList_tmp__filename}
+	docker__docker_repoList_print__filename="docker__docker_repoList.prn"
+	docker__docker_repoList_print__fpath=${docker__tmp__dir}/${docker__docker_repoList_print__filename}
+}
+
+docker__load_constants__sub() {
+	PATTERN_EXITED="Exited"
+}
+
 get_docker_repoList__sub() {
     #Define constants
     local IMAGE_ID="IMAGE-ID"
@@ -106,7 +378,10 @@ get_docker_repoList__sub() {
 			imageSize=`echo ${line_final} | awk '{print $5}'`
 
 			#Check if a container is active for given 'imageID'
-			containerIsActive=`containerIsActive__func "${repoName}" "${tag}"`
+			containerIsActive=$(containerIsActive__func "${repoName}" "${tag}")
+			if [[ -n $(grep -o "${PATTERN_EXITED}.*" <<< "${containerIsActive}") ]]; then
+				containerIsActive="${DOCKER__FG_YELLOW}${containerIsActive}${DOCKER__NOCOLOR}"
+			fi
 
 			#For each object value (e.g., imageID, repoName, tag, createdFor, imageSize, containerIsActive) calculate the longest length
 			#Remark:
@@ -175,6 +450,14 @@ get_docker_repoList__sub() {
 
 #---MAIN SUBROUTINES
 main__sub() {
+	docker__get_source_fullpath__sub
+
+	docker__load_global_fpath_paths__sub
+
+	docker__environmental_variables__sub
+
+	docker__load_constants__sub
+	
     get_docker_repoList__sub
 }
 
